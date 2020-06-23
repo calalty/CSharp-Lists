f3e832d796f63930cf4e2b339fe1cb081b80c4b9
Callum Alton <callum.alton33@gmail.com>
2020-06-23 14:44:04 +0100

    lists

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // arrays: static.
+            // lists: dynamic
+            // use list if you dont know how many items your array is going to hold
+            // userSignup - cant predict how many times a user signs up.
+
+            List<int> salary = new List<int>();
+            int[] otherCosts = new int[] { 9, 12, 10 };
+            salary.Add(15);
+            salary.Add(18000);
+            salary.Add(22000);
+            salary.AddRange(otherCosts);
+            salary.RemoveAt(1);
+            Console.WriteLine(salary.Count);
+            Console.WriteLine(salary);
+            Console.ReadKey();
+        }
+    }
+}
